b452346d074d44c64d5b1c129f05539954fb3d24
bagavathiraja <98267897+bagavathiraja@users.noreply.github.com>
2023-08-19 16:42:00 +0530

    Add files via upload

---
+++ sms/SendSMS.java
@@ -0,0 +1,26 @@
+package sms;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class SendSMS {
+
+	public static void main(String[] args) {
+
+		given().header("x-ibm-client-id","cf79d201-ecca-4f16-b748-167d401e1c1f").header("Content-Type","application/json")
+		.body("{\r\n"
+				+ "    \"Mobile_Number\":\"7010853652\",\r\n"
+				+ "    \"SMS_Text\":\"HELLO.\",\r\n"
+				+ "    \"Template_ID\":\"5\",\r\n"
+				+ "    \"Sender_ID\":\"55555\"\r\n"
+				+ "}\r\n"
+				+ "")
+		.when()
+		.post("https://esbuat.mmfsl.com/mahindrafinance/uat/crm/smsapi/sms")
+		.then()
+		.log().all();
+	    
+	}
+}
