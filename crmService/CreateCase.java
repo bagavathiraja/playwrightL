e5004f8e986ac600d068869b640458dc865ac670
bagavathiraja <98267897+bagavathiraja@users.noreply.github.com>
2023-08-19 16:44:40 +0530

    Add files via upload

---
+++ crmService/CreateCase.java
@@ -0,0 +1,53 @@
+package crmService;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class CreateCase {
+	
+	public static void main(String[] args) {
+
+		given().header("x-ibm-client-id","dc52d3cf-c452-4d27-91e0-68f31d5c7669").header("Origin","uat.quiklyz.com").header("Content-Type","application/json")
+		.body("{\r\n"
+				+ "    \"esbRequest\": {\r\n"
+				+ "        \"header\": {\r\n"
+				+ "            \"apiVersion\": \"1\",\r\n"
+				+ "            \"channelID\": \"LP\",\r\n"
+				+ "            \"deviceID\": \"2\",\r\n"
+				+ "            \"serviceName\": \"Ceatecase\",\r\n"
+				+ "            \"uniqueReqNo\": \"12101120717\",\r\n"
+				+ "            \"timestamp\": \"2022-03-17T00:00:00\"\r\n"
+				+ "        },\r\n"
+				+ "        \"request\": {\r\n"
+				+ "            \"CaseInfoDetails\": {\r\n"
+				+ "                \"CaseDescription\": \"dfhdsfhdshf\",\r\n"
+				+ "                \"CaseType\": \"17\",\r\n"
+				+ "                \"Category\": \"2\",\r\n"
+				+ "                \"CustomerID\": \"8693891582\",\r\n"
+				+ "                \"Email\": \"jd@yahoo.co.in\",\r\n"
+				+ "                \"Channel\": \"2\",\r\n"
+				+ "                \"MobileNumber\": \"8693891582\",\r\n"
+				+ "                \"Name\": \"Samir\",\r\n"
+				+ "                \"SubCaseType\": \"29\",\r\n"
+				+ "                \"ExcessKMRequested\":\"\",\r\n"
+				+ "                \"ExcessTenureRequested\":\"\"\r\n"
+				+ "\r\n"
+				+ "            },\r\n"
+				+ "            \"HeaderInfo\": {\r\n"
+				+ "                \"CallingUser\": \"\",\r\n"
+				+ "                \"Module\": \"\",\r\n"
+				+ "                \"TimeStamp\": \"\",\r\n"
+				+ "                \"UniqueReqno\": \"\"\r\n"
+				+ "            }\r\n"
+				+ "        }\r\n"
+				+ "    }\r\n"
+				+ "}")
+		.when()
+		.post("https://esbuat.mmfsl.com/mahindrafinance/uat/crm/casecreate")
+		.then()
+		.log().all();
+	    
+	}
+}
