c7bd865d9b281ebecfb26af6888fc65794383b8d
bagavathiraja <98267897+bagavathiraja@users.noreply.github.com>
2023-08-22 18:04:51 +0530

    Update GetResults.java

--- posidex/GetResults.java
+++ posidex/GetResults.java
@@ -1,5 +1,7 @@
 package posidex;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -8,7 +10,7 @@ public class GetResults {
 
 	public static void main(String[] args) {
 
-		given().header("x-ibm-client-id","c3543dc4-7b1b-4336-801d-c4e09edd78cd").header("Content-Type","application/json").body("{\r\n"
+		 String response =	given().header("x-ibm-client-id","c3543dc4-7b1b-4336-801d-c4e09edd78cd").header("Content-Type","application/json").body("{\r\n"
 				+ "	\"RequestId\" : \"3131142\",\r\n"
 				+ "	\"SourceAuthenticationToken\": \"posidex\",\r\n"
 				+ "	\"SourceSystemName\": \"admin\"\r\n"
@@ -17,7 +19,26 @@ public class GetResults {
 		.when()
 		.post("https://esbuat.mmfsl.com/mahindrafinance/uat/posidex_dedupe/getresults_v3")
 		.then()
-		.log().all();
-	    
+		//.log().all()
+		.extract().response().asString();
+		 
+		 JsonPath jsonPath = new JsonPath(response);
+	        String name = jsonPath.getString("CustomerResult.Customer[1].Master.Name");
+	        String DOB = jsonPath.getString("CustomerResult.Customer[1].Master.Dob");
+	        String pan = jsonPath.getString("CustomerResult.Customer[15].Master.Pan");
+	        String Address = jsonPath.getString("CustomerResult.Customer[17].Details.Address[0].Address");
+	        String City = jsonPath.getString("CustomerResult.Customer[17].Details.Address[0].City");
+	        String State = jsonPath.getString("CustomerResult.Customer[17].Details.Address[0].State");
+	        String Pin = jsonPath.getString("CustomerResult.Customer[17].Details.Address[0].Pin");
+	        
+	     
+
+	        System.out.println("Name: " + name);
+	        System.out.println("DOB: " + DOB);
+	        System.out.println("Pan: " + pan);
+	        System.out.println("State: " + Address);
+	        System.out.println("City: " + City);
+		 
+    
 	}
 }
